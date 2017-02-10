57b05895ce67f89b78da9ffd89dd76de7fc154cf
amillikin <amillikin@AARON-SURFACE>
2017-02-09 20:25:18 -0500

    morefailure

--- DES/DES.cpp
+++ DES/DES.cpp
@@ -6,8 +6,11 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
+#include <climits>
 using namespace std;
 int keyType;
+typedef unsigned long long BIG;
 
 //Converts a string to all uppercase characters - ARM
 string upCase(string str) {
@@ -70,15 +73,20 @@ bool validMode(string mode) {
 		return false;
 	}
 }
-
+BIG PC_1(BIG bitString) {
+	return 1;
+}
 int main()
 {
 	string strIn, cmd, action, key, mode, inFilename, outFilename;
+	streampos begF, endF, size;
+	BIG x;
 	ifstream inFile;
 	ofstream outFile;
 	bool validEntry = false;
 	cout << "Welcome to Aaron's DES Encrypter/Decrypter!" << endl;
-	while (!validEntry) {
+	/*while (!validEntry) {
+	LoopAgain:
 		cout << "Accepted input: DES <-action> <key> <mode> <infile> <outfile>" << endl;
 		getline(cin, strIn);
 		cmd = strIn.substr(0, 4);
@@ -86,11 +94,11 @@ int main()
 			strIn = strIn.substr(4, strIn.length());
 			action = upCase(strIn.substr(0, 3));
 			if (validAction(action)) {
-				action = action.substr(1, 2);
+				action = action.substr(1, 1);
 				strIn = strIn.substr(3, strIn.length());
 				keyType = getKeyType(strIn);
 				if (keyType == 0) {
-					break;
+					goto LoopAgain;
 				}
 				else {
 					if (keyType == 1) {
@@ -105,27 +113,64 @@ int main()
 						key = strIn.substr(2, 8);
 						strIn = strIn.substr(13, strIn.length());
 					}
-					if (validMode(strIn)) {
-						mode = strIn.substr(0, 3);
-						strIn = strIn.substr(4, strIn.length());
-						inFilename = strIn.substr(0, strIn.find_first_of(" "));
-						inFile.open(inFilename);
+					mode = upCase(strIn.substr(0, 4));
+					strIn = strIn.substr(4, strIn.length());
+					if (validMode(mode)) {
+						mode = mode.substr(0, 3);
+						//inFilename = strIn.substr(0, strIn.find_first_of(" "));
+						//inFilename = "C:/Users/amillikin/Documents/Visual Studio 2015/Projects/DES/Debug/DEStest.txt";
+						inFilename = "C:\\Users\\amillikin\\Desktop\\Spring 17\\EECS 4980 - Inside Cryptography\\Project 1\\Shakespeare.txt";
+						inFile.open(inFilename, inFile.binary);
 						if (!inFile) {
 							cout << "Can't open input file " << inFilename << endl;
-							break;
+							goto LoopAgain;
 						}
 						strIn = strIn.substr(strIn.find_first_of(" "), strIn.length());
-						outFilename = strIn.substr(1, strIn.find_first_of(" "));
-						outFile.open(outFilename);
+						//outFilename = strIn.substr(1, strIn.find_first_of(" "));
+						outFilename = "C:/Users/amillikin/Documents/Visual Studio 2015/Projects/DES/Debug/DESout.txt";
+						outFile.open(outFilename, outFile.app);
 						if (!outFile) {
 							cout << "Can't open output file " << outFilename << endl;
-							break;
+							goto LoopAgain;
 						}
-						validEntry = true;
+						inFile.seekg(0, inFile.beg);
+						begF = inFile.tellg();
+						inFile.seekg(0, inFile.end);
+						endF = inFile.tellg();
+						fileSize = endF - begF;
+						if (fileSize > 2147483648) {
+							cout << "File too large." << endl;
+							goto LoopAgain;
+						}
+						else if (fileSize == 0) {
+							cout << "Nothing to encrypt/decrypt." << endl;
+							goto LoopAgain;
+						}
+						else {
+							validEntry = true;
+						}
 					}
 				}
 			}
+
 		}
+
+
+	}*/
+	inFilename = "C:\\Users\\amillikin\\Desktop\\Spring 17\\EECS 4980 - Inside Cryptography\\Project 1\\Shakespeare.txt";
+	inFile.open(inFilename, inFile.binary);
+	begF = inFile.tellg();
+	inFile.seekg(0, inFile.end);
+	endF = inFile.tellg();
+	size = endF - begF;
+	cout << size << endl;
+	inFile.seekg(0, inFile.beg);
+	for (BIG buffer; inFile.read(reinterpret_cast<char*>(&buffer), sizeof buffer);) {
+		cout << inFile.gcount() << endl;
+		if (inFile.gcount() < 64) {
+			cout << inFile.gcount() << endl;
+		}
+		cout << setw(sizeof buffer * CHAR_BIT) << buffer << endl;
 	}
 	return 0;
 }
